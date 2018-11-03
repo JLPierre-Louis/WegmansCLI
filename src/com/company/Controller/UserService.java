da91bd8688953814e1f6d03dfb50ba9e8014bda5
rxc3202 <rxc3202@rit.edu>
2018-11-03 12:56:50 -0400

    stubbed out controller tier

--- src/com/company/Controller/UserService.java
+++ src/com/company/Controller/UserService.java
@@ -1,5 +1,48 @@
 package com.company.Controller;
 
+import com.company.Model.Store;
+import com.company.Model.User;
+import com.company.View.Prompt;
+
 public class UserService {
+
+    enum UserType {
+        admin, customer
+    }
+
+    private Prompt currentPrompt;
+    private User currentUser;
+    private Store currentStore;
+
+    public UserService(User user, Prompt prompt) {
+        currentPrompt = prompt;
+        currentUser = user;
+        currentStore = null;
+    }
+
+    public Store getCurrentStore() {
+        return currentStore;
+    }
+
+    public Prompt getCurrentPrompt() {
+        return currentPrompt;
+    }
+
+    public void updatePrompt(Prompt newPrompt) {
+
+    }
+
+    public void switchToStore(Store store) {
+
+    }
+
+    public Prompt login() {
+        return null;
+    }
+
+    public void logOut() {
+
+    }
+
 
 }
--- src/com/company/Controller/WegmansCLI.java
+++ src/com/company/Controller/WegmansCLI.java
@@ -1,5 +1,27 @@
 package com.company.Controller;
 
+import com.company.Model.Store;
+import com.company.Model.User;
+import java.util.HashMap;
+
 public class WegmansCLI {
+
+    private HashMap<User, UserService> currentUsers;
+
+    public WegmansCLI() {
+        currentUsers = new HashMap<>();
+    }
+
+    public void initDatabase() {
+
+    }
+
+    public void setUserStore(User user, Store store) {
+
+    }
+
+    public void run() {
+
+    }
 
 }
