b04fe26f8af4476542868a3469d98efcba1d2864
jaf9897 <jaf9897@rit.edu>
2018-11-26 14:46:16 -0500

    Forget to push the .jar library that makes the SQL code possible, also added test main to SQLConnection.

--- src/com/company/Controller/SQLConnection.java
+++ src/com/company/Controller/SQLConnection.java
@@ -55,4 +55,31 @@ public class SQLConnection {
         return rs;
     }
 
+    public static void main(String[] args) {
+        SQLConnection con = new SQLConnection();
+        Connection db = null;
+        ResultSetMetaData rsmd = null;
+        try {
+            db = con.connectToDB("wegmans2");
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        ResultSet rs = wegmansQuery(db, "SELECT * FROM product");
+        try {
+            rsmd = rs.getMetaData();
+
+            int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = rs.getString(i);
+                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                }
+                System.out.println("");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
