cdb0c97e818a0218cc8dc31bc7badc8a2591d090
Jorge <jaf9897@rit.edu>
2018-12-04 00:50:26 -0500

    Added fully functional reorder fulfillment. Updates stock in appropriate store and updates reorder table with current date.

--- src/com/company/Model/Admin.java
+++ src/com/company/Model/Admin.java
@@ -1,11 +1,10 @@
 package com.company.Model;
 
 import com.company.Controller.SQLConnection;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Random;
 
 
@@ -16,7 +15,14 @@ public class Admin extends User {
     private final String CREATE_REORDER_REQUEST = "INSERT INTO Reorder (orderNumber, product, store, stockRequested)" +
             "                                      VALUES (?, ?, ?, ?)";
     private final String ORDER_NUMBERS = "SELECT ordernumber FROM Reorder";
-    private final String GET_UNFULFILLED_ORDERS = "SELECT product, store, stockRequested FROM Reorder WHERE deliveryDate IS NULL";
+    private final String GET_UNFULFILLED_ORDERS = "SELECT ordernumber, product, store, stockRequested FROM Reorder" +
+            " WHERE deliveryDate IS NULL";
+    private final String UPDATE_STOCK = "UPDATE soldby SET numberinstock = ((SELECT stockrequested FROM " +
+            "reorder WHERE store = ? AND product = ?) + (SELECT numberInStock FROM soldBy WHERE storeId = ? AND " +
+            "productId = ?)) WHERE storeId = ? AND productId = ?";
+    private final String UPDATE_REORDER_TABLE = "UPDATE reorder SET deliverydate = ?, fulfilledBy = (SELECT " +
+            "distributedBy.vendor FROM distributedBy JOIN Product ON distributedBy.brand = product.brand " +
+            "WHERE product.upc = ?) WHERE ordernumber = ?";
     private String username;
 
     public Admin(String username){
@@ -58,6 +64,30 @@ public class Admin extends User {
         try {
             PreparedStatement stmt = this.getCon().prepareStatement(GET_UNFULFILLED_ORDERS);
             ResultSet rs = stmt.executeQuery();
+            Date date = new Date(Calendar.getInstance().getTime().getTime());
+            if (!rs.next()) {
+                System.out.println("No unfulfilled reorders!");
+            } else {
+                do {
+                    String orderNumber = rs.getString(1);
+                    String upc = rs.getString(2);
+                    String storeID = rs.getString(3);
+                    int stockRequested = rs.getInt(4);
+                    stmt = this.getCon().prepareStatement(UPDATE_STOCK);
+                    stmt.setString(1, storeID);
+                    stmt.setString(2, upc);
+                    stmt.setString(3, storeID);
+                    stmt.setString(4, upc);
+                    stmt.setString(5, storeID);
+                    stmt.setString(6, upc);
+                    stmt.executeUpdate();
+
+                    stmt = this.getCon().prepareStatement(UPDATE_REORDER_TABLE);
+                    stmt.setDate(1, date);
+                    stmt.setString(2, upc);
+                    stmt.setString(3, orderNumber);
+                    stmt.executeUpdate();
+                }while (rs.next()); }
         } catch (SQLException e){
             System.out.println("Error while fulfilling reorder");
             e.printStackTrace();
