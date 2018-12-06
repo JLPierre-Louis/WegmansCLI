3fe6f2be7676eab9907ccabeaf7b7af172885815
jaf9897 <jaf9897@rit.edu>
2018-12-06 00:18:14 -0500

    Applied fancy product print to the main product resultset print function.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,6 @@ import com.company.Model.Admin;
 public class Main {
 
     public static void main(String[] args) {
-        Admin a = new Admin("asdd");
-        a.selectMainStore("1");
-        a.getStoreInventory();
         WegmansCLI main = new WegmansCLI();
         main.run();
     }
